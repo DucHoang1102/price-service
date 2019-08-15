5a2940288926a3daf4701eada0435805b948e79e
DucHoang <ndhdldt1@gmail.com>
2019-08-15 11:36:55 +0700

    Complete controllers/price.js

--- controllers/price.js
+++ controllers/price.js
@@ -9,6 +9,26 @@ exports.index = function (req, res, next) {
 };
 
 exports.view = function (req, res, next) {
+    var limit  = String(req.body.limit)  || 20; // Why String()? Because case req.body.limit = 0 is Number
+    var offset = String(req.body.offset) || 0;
+    var query  = req.body.query          || {};
+    var sort   = req.body.sort           || {createdAt: 'desc'};
+    var select = req.body.select         || '';
+
+    var results = Price.find(query)
+        .select(select)
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .sort(sort)
+        .exec();
+
+    Promise.all([results]).then(results => {
+        var prices = results[0]
+        return res.json({
+            prices: prices
+        });
+
+    }).catch( err => res.json({ errors: err.message }) );
 };
 
 exports.new = function (req, res, next) {
@@ -23,53 +43,61 @@ exports.new = function (req, res, next) {
 };
 
 exports.details = function (req, res, next) {
-    Price.findById('5d51bcf46716902cd408a873').then(result => {
-        var re = new RegExp(result.regexp, 'i');
+    Price.findById(req.params.id).then(result => {
+        if (!result) throw new Error('Price not found');
 
-        var datas = [
-            {
-                should: 'True',
-                str   : 'A-34445jhdddd343-CT1-S-2'
-            },
-            {
-                should: 'False',
-                str   : 'B-34445jhdddd343-CT1'
-            },
-            {
-                should: 'False', 
-                str   : 'B-A34445jhdddd343-CT2'
-            },
-            {
-                should: 'False',
-                str   : 'B-34445jhdddd343-'
-            },
-            {
-                should: 'True',
-                str   : 'A-efdeef1dfe-CT1-XL-2'
-            },
-            {
-                should: 'True',
-                str   : 'A-123123ddd-CT1-XXXXL-2'
-            },
-        ]
+        return res.json({
+            prices: result
+        });
 
-        for (let data of datas)
-        {
-            console.log(data.should + '----' + re.test(data.str));
-        }
-
-        return res.json({});
     }).catch( err => res.json({ errors: err.message }) );
 };
 
 exports.update = function (req, res, next) {
-    return res.json({
-        message: 'Ok thank you'
-    });
+    Price.findById(req.params.id).then(price => {
+        if (!price) throw Error('Price not found');
+
+        if (typeof req.body.price.name !== 'undefined') {
+            price.name = req.body.price.name;
+        }
+
+        if (typeof req.body.price.description !== 'undefined') {
+            price.description = req.body.price.description;
+        }
+
+        if (typeof req.body.price.regexp !== 'undefined') {
+            price.regexp = req.body.price.regexp;
+        }
+
+        if (typeof req.body.price.group !== 'undefined') {
+            price.group = req.body.price.group;
+        }
+
+        if (typeof req.body.price.layer !== 'undefined') {
+            price.layer = req.body.price.layer;
+        }
+
+        if (typeof req.body.price.active !== 'undefined') {
+            price.active = req.body.price.active;
+        }
+
+        price.save().then(price => {
+            return res.json({
+                prices: price
+            });
+
+        }).catch( err => res.json({ errors: err.message }) );
+
+    }).catch( err => res.json({ errors: err.message }) );
 };
 
 exports.delete = function (req, res, next) {
-    return res.json({
-        message: 'Ok thank you'
-    });
+    Price.findByIdAndRemove(req.params.id).then(result => {
+        if (!result) throw new Error('Price not found');
+
+        return res.json({
+            prices: result
+        });
+
+    }).catch( err => res.json({ errors: err.message }) );
 };
--- models/Price.js
+++ models/Price.js
@@ -2,10 +2,12 @@ var mongoose        = require('mongoose'),
     uniqueValidator = require('mongoose-unique-validator');
 
 var PriceSchema = new mongoose.Schema({
-    regexp : { type: String, trim: true },
-    group  : { type: mongoose.Schema.Types.ObjectId },
-    layer  : { type: Number},
-    active : { type: Number, min: 0, max: 1, default: 1}
+    name       : { type: String },
+    description: { type: String },
+    regexp     : { type: String, trim: true },
+    group      : { type: mongoose.Schema.Types.ObjectId },
+    layer      : { type: Number},
+    active     : { type: Number, min: 0, max: 1, default: 1}
 
 }, { timestamps: true } );
 

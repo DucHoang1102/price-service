3407975e81668173bf44d60aefe57fbc61a40e7c
DucHoang <ndhdldt1@gmail.com>
2019-10-28 15:07:28 +0700

    Edit layer 0 -> 1

--- models/Price.js
+++ models/Price.js
@@ -6,7 +6,7 @@ var PriceSchema = new mongoose.Schema({
     description: { type: String },
     regexp     : { type: String, trim: true },
     value      : { type: Number, min: 0, default: 0 },
-    layer      : { type: Number, min: 0, default: 0 },
+    layer      : { type: Number, min: 1, default: 1 },
     active     : { type: Number, min: 0, max: 1, default: 1 },
     group      : { type: mongoose.Schema.Types.ObjectId }
 
